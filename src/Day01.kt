7c86d28f931a427c828af562ca89c0111dec537a
Peter Šuly <peter.suly@goodrequest.com>
2022-12-01 16:32:17 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+  fun part1(input: List<String>): Int {
+    return getSnackSum(input).max()
+  }
+
+  fun part2(input: List<String>): Int {
+    return getSnackSum(input).sortedDescending().take(3).sum()
+  }
+
+  val testInput = readInput("Day01.txt")
+  println(part1(testInput))
+  println(part2(testInput))
+}
+
+private fun getSnackSum(input: List<String>) = input.chunkedByPredicate(String::isBlank).map { it.sumOf(String::toInt) }
+
+private fun <T> List<T>.chunkedByPredicate(predicate : (T) -> Boolean): List<List<T>> = buildList {
+  this@chunkedByPredicate.fold(emptyList<T>()) { acc, value ->
+    if (predicate(value).not()) {
+      acc + value
+    } else {
+      add(acc)
+      emptyList()
     }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+  }
 }
